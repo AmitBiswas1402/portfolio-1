66427922286ae28e657014137b708bc5a0de5d82
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-08-06 16:43:25 +0530

    Hero and Profile and links made

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className="mx-auto max-w-4xl px-4 sm:px-6 lg:px-8 p-2">
+    <main className="mx-auto max-w-4xl px-4 sm:px-6 lg:px-8 p-4">
       <Hero />
     </main>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,37 +1,82 @@
+"use client";
+import { MapPin } from "lucide-react";
 import Image from "next/image";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
+import { PiReadCvLogo } from "react-icons/pi";
 
 const Hero = () => {
   return (
-    <main>
-      <div className="">
-        <div className="flex items-center">
-          <Image
-            src="/pic.png"
-            height={120}
-            width={120}
-            className="rounded-full"
-            alt=""
-          />
-          <div className="text-xl font-semibold">Name</div>
-          <div className="text-sm text-gray-400">Location</div>
-          <div className="flex gap-4 text-sm text-blue-500">
-            <a href="#">LinkedIn</a>
-            <a href="#">GitHub</a>
-            <a href="#">Twitter</a>
+    <div className="m-4">
+      <div className="flex">
+        <Image
+          src="/pic.png"
+          className="rounded-full"
+          width={140}
+          height={140}
+          alt="Profile picture"
+        />
+        <div className="m-4">
+          <div className="text-3xl font-bold mb-2">Amit Biswas</div>
+          <div className="flex text-slate-400">
+            <MapPin /> <span className="ml-2 text-l">Kolkata, India</span>
           </div>
 
-        </div>
+          <div className="mt-4 flex gap-5 flex-wrap text-white">
+            {/* LinkedIn */}
+            <a
+              href="https://www.linkedin.com/in/amitbiswas1402/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center hover:text-blue-400"
+            >
+              <FaLinkedin size={20} />
+              <span className="ml-2">LinkedIn</span>
+            </a>
 
-        <div className="space-y-1">
+            {/* GitHub */}
+            <a
+              href="https://github.com/AmitBiswas1402/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center hover:text-gray-400"
+            >
+              <FaGithub size={20} />
+              <span className="ml-2">GitHub</span>
+            </a>
+
+            {/* LeetCode */}
+            <a
+              href="https://leetcode.com/u/AmitBiswas1402/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center hover:text-yellow-400"
+            >
+              <SiLeetcode size={20} />
+              <span className="ml-2">Leetcode</span>
+            </a>
+
+            {/* Resume */}
+            <a
+              href="https://docs.google.com/document/d/1Su8xoLLqCohXuoB6f-e_kI1F0VuYT0vxstArHKtVXtE/edit?usp=sharing"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center hover:text-green-400"
+            >
+              <PiReadCvLogo size={20} />
+              <span className="ml-2">Resume</span>
+            </a>
+          </div>
         </div>
       </div>
 
-      <div className="">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus totam eum
-        ratione accusamus quam blanditiis magni, odit perspiciatis placeat
-        labore?
+      {/* Paragraph with margin top */}
+      <div className="mt-6 m-2 text-slate-300">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto laudantium ratione perspiciatis quo odit vero, minus exercitationem aut possimus tenetur doloremque sint veritatis aliquid numquam nostrum, dolor magni nam omnis!
       </div>
-    </main>
+    </div>
   );
 };
+
 export default Hero;
